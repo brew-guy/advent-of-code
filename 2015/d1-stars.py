e1194e2247a99c3679d5623ac56a09838322f21c
brew-guy <jes.hunsballe@gmail.com>
2021-11-14 22:04:45 +0100

    Adds year 2015 day 1 code attempt.

---
+++ 2015/d1-stars.py
@@ -0,0 +1,20 @@
+import os, sys
+
+path = os.path.join(sys.path[0], "d1-input-1.txt")
+with open(path, "r") as fp:
+    lines = fp.readlines()
+
+# Part 1
+directions = lines[0]
+floor = directions.count("(") - directions.count(")")
+print(f"Star 1: {floor}")
+
+# Part 2
+dirs = {"(": 1, ")": -1}
+floor = 0
+for index, direction in enumerate(directions):
+    floor += dirs[direction]
+    if floor == -1:
+        break
+
+print(f"Star 2: {index + 1}")
