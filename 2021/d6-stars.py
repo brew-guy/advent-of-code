acb160881507140f4bd13d284b4f1eb578ad06a3
Jes <jes.hunsballe@gmail.com>
2021-12-08 22:23:56 +0100

    Fast solution for Day 6

--- 2021/d6-stars.py
+++ 2021/d6-stars.py
@@ -7,7 +7,10 @@ input = mypath + "d6-input.txt"
 with open(input) as f:
     fishes = [int(fish) for fish in f.read().split(",")]
 
+sample = 3, 4, 3, 1, 2
+
 # Part 1
+# Baaad solution! Go away! Shoo!
 def recursiveBreeding(shoal, generations):
     new_shoal, new_born = [], []
     if generations < 1:
@@ -26,5 +29,13 @@ def recursiveBreeding(shoal, generations):
 dropstar(11, recursiveBreeding(fishes, 80), t)
 
 # Part 2
+def fastGrowth(shoal, generations):
+    count = [shoal.count(f) for f in range(9)]  # Fish count per index
+    for g in range(generations):
+        new_fish = count[:1]
+        count = count[1:] + new_fish  # Left rotate list
+        count[6] += new_fish[0]
+    return sum(count)
 
-dropstar(12, recursiveBreeding(fishes, 256), t)
+
+dropstar(12, fastGrowth(fishes, 256), t)
