40e8917bf75e13b513fbe416ec9967f5fb767f24
brew-guy <jes.hunsballe@gmail.com>
2021-12-17 21:58:46 +0100

    Adds day 15 part 1. To-do: find better Dijkstra/BFS for grid...

--- 2015/test.py
+++ 2015/test.py
@@ -1,18 +1,9 @@
-n = 5
+def evaluate(cell):
+    operation, arg_1, arg_2 = cell
 
 
-def sumInts(first, last):
-    n = last - first + 1
-    return n * (first + last) / 2
+a = ["jmp", "reg1", 4]
+evaluate(a)
 
-
-def sumIters(first, n):
-    return n * (n + 2 * first - 1) / 2
-
-
-r, c = 2, 5
-i = sumInts(1, c)
-j = summie(c, r - 1)
-
-print(i, j)
-
+b = ["jmp", "reg1"]
+evaluate(b)
